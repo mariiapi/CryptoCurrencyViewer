dbae09c7ba027c69fa4eaedeccf61c756e02d7e3
mariiapi <mashapisnya@gmail.com>
2024-12-10 15:28:09 +0200

    visuals

--- app/src/main/java/com/example/cryptocurrencyviewer/di/CryptoApplication.kt
+++ app/src/main/java/com/example/cryptocurrencyviewer/di/CryptoApplication.kt
@@ -1,6 +1,7 @@
 package com.example.cryptocurrencyviewer.di
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
